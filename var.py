221e79fee4fec19a999fa27662d866c3457b3a13
ak4422920 <ak4422920@gmail.com>
2025-04-12 09:18:18 +0530

    Update var.py

--- var.py
+++ var.py
@@ -2,20 +2,20 @@
 
 import os
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "29171167")
+API_HASH = os.getenv("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN = int(os.getenv("ADMIN", ""))
+ADMIN = int(os.getenv("ADMIN", "7251898668"))
 
-CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/DypixxTech")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100xxxxxxx"))
-DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-100xxxxxxx"))
+CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/Cineoriginals")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002264638393"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002264638393"))
 
-DB_URI = os.getenv("DB_URI", "mongodb+srv://xxxx:xxxx@xxxx")
-DB_NAME = os.getenv("DB_NAME", "instalaoder")
+DB_URI = os.getenv("DB_URI", "mongodb+srv://tg:tg@cluster0.fekvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.getenv("DB_NAME", "cluster0")
 
 IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-100xxxxxxx") # Add Multiple Channels iD By Space
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002442422204") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
 
 
